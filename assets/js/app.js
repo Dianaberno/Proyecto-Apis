55344774b39f4cab5cd0e386174e66820bbbf8b7
paola <bernodiana@gmail.com>
2017-09-12 16:04:29 -0500

    hize cambios con el javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,40 @@
 function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -12.1191427, lng: -77.0349046 },
+        zoom: 6
+    });
+    var infoWindow = new google.maps.InfoWindow({ map: map });
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+map.setZoom(18);
+map.setCenter({ lat: latitud, lng: longitud });
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+}
+document.getElementById("encuentrame").addEventListener("click", buscar);
+/*function initMap() {
     var laboratoriaLima = { lat: -12.1191427, lng: -77.0349046 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 18,
@@ -30,4 +66,4 @@ map.setCenter({ lat: latitud, lng: longitud });
 var funcionError = function(error) {
     alert("Tenemos un problema con encontrar tu ubicacion");
 }
-document.getElementById("encuentrame").addEventListener("click", buscar);
+document.getElementById("encuentrame").addEventListener("click", buscar);*/
